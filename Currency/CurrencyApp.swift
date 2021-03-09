7dd41d4290c25ac1ad7ee8640195a037377c57b1
David Garcia Tort <Degete_@hotmail.com>
2021-03-09 11:38:19 +0900

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by David Garcia Tort on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
